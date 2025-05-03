54c3510718084da12b541296e446ad54a2e40d0f
Noé Godin <noegodin2@gmail.com>
2025-05-03 10:40:02 +0200

    feat : technologies update

--- src/data/technologies.jsx
+++ src/data/technologies.jsx
@@ -1,43 +1,54 @@
-import FigmaIcon from "@mui/icons-material/Brush";
-import TypeScriptIcon from "@mui/icons-material/Code";
-import { FaJava, FaReact } from "react-icons/fa";
+import { FaJava, FaReact, FaDocker, FaPython } from "react-icons/fa";
 import { VscAzure } from "react-icons/vsc";
 import { ImGit } from "react-icons/im";
+import { SiCplusplus, SiTypescript } from "react-icons/si";
 
 const technologies = [
   {
-    name: "Figma",
-    description: "Design Tool",
-    icon: <FigmaIcon />,
-    color: "rgba(31, 117, 87, 0.2)",
-  },
-  {
-    name: "TypeScript",
-    description: "JavaScript but better",
-    icon: <TypeScriptIcon />,
-    color: "rgba(49, 120, 198, 0.2)",
-  },
-  {
     name: "React",
-    description: "JavaScript Library",
+    description: "JavaScript component library",
     icon: <FaReact size={24} />,
     color: "rgba(97, 218, 251, 0.8)",
   },
   {
-    name: "Azure",
-    description: "Cloud service provider",
-    icon: <VscAzure size={24} />,
-    color: "rgba(8, 100, 180, 0.8)",
-  },
-  {
     name: "Java",
-    description: "Programming language",
+    description: "Enterprise-grade language",
     icon: <FaJava size={24} />,
     color: "rgba(234, 45, 46, 0.8)",
   },
   {
+    name: "TypeScript",
+    description: "Typed JavaScript superset",
+    icon: <SiTypescript size={24} />,
+    color: "rgba(49, 120, 198, 0.8)",
+  },
+  {
+    name: "C++",
+    description: "High-performance language",
+    icon: <SiCplusplus size={24} />,
+    color: "rgba(101, 154, 210, 0.8)",
+  },
+  {
+    name: "Python",
+    description: "Versatile scripting language",
+    icon: <FaPython size={24} />,
+    color: "rgba(255, 219, 79, 0.8)",
+  },
+  {
+    name: "Azure",
+    description: "Microsoft cloud platform",
+    icon: <VscAzure size={24} />,
+    color: "rgba(8, 100, 180, 0.8)",
+  },
+  {
+    name: "Docker",
+    description: "Container service provider",
+    icon: <FaDocker size={24} />,
+    color: "rgba(36, 150, 237, 0.8)",
+  },
+  {
     name: "Git",
-    description: "Version control",
+    description: "Distributed version control",
     icon: <ImGit size={24} />,
     color: "rgba(242, 80, 55, 0.8)",
   },
